67ce054572cac5e91d47e68e53a5e3faeb5a7bf8
Roman <roman.ivanov.dev@gmail.com>
2023-08-08 17:45:45 +0300

    Uploading old project.

---
+++ README.md
@@ -0,0 +1,41 @@
+# To-do app
+
+This is my old todo app, written with JavaScript and featuring MongoDB, Express.js, Bootstrap, and EJS
+
+## Environment Variables
+
+To run this project, you will need to add the following environment variables to your .env file. You can also copy it from .env_example.
+
+`MONGO_URI` - Your mongodb connection string
+
+`DATABASE_NAME = 'TODO'` - Database name
+
+`SESSION_SECRET` - Secret used to sign the session cookie
+
+`PORT` - Server port
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/JustEnough1/To-do-app.git
+```
+
+Install dependencies
+
+```bash
+  npm i
+```
+
+Add environment variables
+
+```bash
+  Rename .env_example to .env and set values
+```
+
+Start the server
+
+```bash
+  npm start
+```
---
+++ database.js
@@ -0,0 +1,52 @@
+const { MongoClient } = require("mongodb");
+
+const process = require("process");
+require("dotenv").config()
+
+const MONGO_URI = process.env.MONGO_URI;
+const DATABASE_NAME = process.env.DATABASE_NAME;
+
+const client = new MongoClient(MONGO_URI);
+
+class DatabaseHelper {
+  constructor() { }
+
+  async initialize() {
+    try {
+      await client.connect();
+      this.database = client.db(DATABASE_NAME);
+
+      const isUsers = await this.database
+        .listCollections({ name: "users" })
+        .hasNext();
+      const isTasks = await this.database
+        .listCollections({ name: "tasks" })
+        .hasNext();
+
+      if (!isUsers) this.database.createCollection("users");
+      if (!isTasks) this.database.createCollection("tasks");
+
+      console.log("Connected to database");
+    } catch (error) {
+      console.log("Error occured");
+    }
+  }
+
+  getDatabase() {
+    if (!this.database) {
+      console.log("Not initilized yet");
+      return;
+    }
+    return this.database;
+  }
+}
+
+process.on("SIGINT", async () => {
+  await client.close();
+  console.log("MongoDB disconnected on app termination");
+  process.exit(0);
+});
+
+module.exports = {
+  DatabaseHelper,
+};
